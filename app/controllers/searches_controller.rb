d5e88a6cb3d06bc952f3776863ca2e4581c530a7
Boris Stukov <stukov.boris@gmail.com>
2017-04-29 13:00:15 +0600

    Fix search, fix buy tickets

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,6 +4,8 @@ class SearchesController < ApplicationController
 
   def create
     @trains = Search.trains(search_params[:start_station_id], search_params[:end_station_id])
+    @start_station = search_params[:start_station_id]
+    @end_station = search_params[:end_station_id]
     render :show
   end
 
