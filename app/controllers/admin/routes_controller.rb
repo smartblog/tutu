bc25395f76e32b5b9ec7bb817505c089cb84c53f
Boris Stukov <stukov.boris@gmail.com>
2017-05-01 18:48:35 +0600

    Fix paths routes

--- app/controllers/admin/routes_controller.rb
+++ app/controllers/admin/routes_controller.rb
@@ -14,9 +14,8 @@ class Admin::RoutesController < Admin::BaseController
 
   def create
     @route = Route.new(route_params)
-
     if @route.save
-      redirect_to @route
+      redirect_to [:admin, @route]
     else
       render :new
     end
@@ -27,7 +26,7 @@ class Admin::RoutesController < Admin::BaseController
 
   def update
     if @route.update(route_params)
-      redirect_to @route
+      redirect_to [:admin, @route]
     else
       render :edit
     end
@@ -35,7 +34,7 @@ class Admin::RoutesController < Admin::BaseController
 
   def destroy
     @route.destroy
-    redirect_to routes_path
+    redirect_to admin_routes_path
   end
 
   private
