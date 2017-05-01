2708c8c830d25779a5ac5136c2dc52d931dc214f
Boris Stukov <stukov.boris@gmail.com>
2017-05-01 18:24:51 +0600

    Add First and Last names to users and registrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430115840) do
+ActiveRecord::Schema.define(version: 20170501115403) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number"
@@ -87,6 +87,8 @@ ActiveRecord::Schema.define(version: 20170430115840) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.boolean  "admin",                  default: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
