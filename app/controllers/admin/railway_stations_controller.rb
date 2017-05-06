1e4bfdfa5a3474cba6d935fa2dc8bc053f42e4a6
Boris Stukov <stukov.boris@gmail.com>
2017-05-06 16:47:29 +0600

    Fix update stations in route

--- app/controllers/admin/railway_stations_controller.rb
+++ app/controllers/admin/railway_stations_controller.rb
@@ -40,13 +40,13 @@ class Admin::RailwayStationsController < Admin::BaseController
   def update_position
     @route = Route.find(params[:route_id])
     @railway_station.update_position(@route, params[:position])
-    redirect_to @route, notice: 'Position of RailwayStation was successfully updated.'
+    redirect_to admin_route_path(@route), notice: 'Position of RailwayStation was successfully updated.'
   end
 
   def update_times
     @route = Route.find(params[:route_id])
     @railway_station.update_times(@route, params[:arrive_time], params[:departure_time])
-    redirect_to @route, notice: 'Times for RailwayStation in Route was successfully updated.'
+    redirect_to admin_route_path(@route), notice: 'Times for RailwayStation in Route was successfully updated.'
   end
 
   private
