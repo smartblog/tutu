b86952ecee0569f82f477a59990082caa20f67be
Boris Stukov <stukov.boris@gmail.com>
2017-05-06 13:24:26 +0600

    Create mail notification to buy tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,16 @@ class Ticket < ActiveRecord::Base
   belongs_to :begin_station, class_name: 'RailwayStation', foreign_key: :begin_station_id
   belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
   belongs_to :user, optional: true
+
+  after_create :send_notification
+
+  def route_name
+    "#{begin_station.title} - #{end_station.title}"
+  end
+
+  private
+
+  def send_notification
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501115403) do
+ActiveRecord::Schema.define(version: 20170506065832) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170501115403) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "train_id"
+    t.index ["id", "type"], name: "index_carriages_on_id_and_type"
     t.index ["train_id"], name: "index_carriages_on_train_id"
   end
 
