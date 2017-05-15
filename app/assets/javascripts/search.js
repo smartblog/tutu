eb31e873fc06f8fe50ca03c33f7adc57606f3407
Boris Stukov <stukov.boris@gmail.com>
2017-05-15 13:25:39 +0600

    Add JS to search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  alert('Страница загружена');
+  $('.search_form').submit(function() {
+    var start_station;
+    var end_station;
+
+    start_station = $(this).find('#stations_start_station_id');
+    end_station = $(this).find('#stations_end_station_id');
+
+    if (start_station.val() == end_station.val()) {
+      alert('Choice End station');
+      return false;
+    }
+  })
 });
